3cd8d64bd5eaefd3f7f218237667ac2a990e92c8
ismoilovdevml <ismoilovdevarchlinux@gmail.com>
2022-11-13 02:14:12 +0500

    updated model

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
-import streamlit as st
-import numpy as np
+import streamlit as st  
+import numpy as np 
 
-import matplotlib.pyplot as plt
-from sklearn import datasets
-from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt 
+from sklearn import datasets 
+from sklearn.model_selection import train_test_split  
 
-from sklearn.decomposition import PCA
-from sklearn.svm import SVC
-from sklearn.neighbors import KNeighborsClassifier
-from skleaarn.ensemble import RandomForestClassifier
+from sklearn.decomposition import PCA 
+from sklearn.svm import SVC 
+from sklearn.neighbors import KNeighborsClassifier 
+from sklearn.ensemble import RandomForestClassifier  
 
 from sklearn.metrics import accuracy_score
+
+st.title('Cancer Detection')
+st.subheader('Cancer Detection Machine Learning Model')
+
+data = datasets.load_breast_cancer()
+X = data.data 
+y = data.target
+
+st.write('X shakli:', X.shape)
+st.write('Maqsadli raqam:', len(np.unique(y)))
