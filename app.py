71c8c4a51d1ac5e0c3a9773f6db80907d0464488
ismoilovdevml <ismoilovdevarchlinux@gmail.com>
2022-11-13 02:28:09 +0500

    update model

--- app.py
+++ app.py
@@ -21,3 +21,24 @@ y = data.target
 
 st.write('X shakli:', X.shape)
 st.write('Maqsadli raqam:', len(np.unique(y)))
+
+import pandas as pd 
+df = pd.DataFrame(X, columns=data.feature_names) 
+df[:5]
+
+clf_name = st.sidebar.selectbox(
+        'Machine Learning algoritmni tanlang',
+        ('KNN', 'SVM', 'Random Forest')
+)
+
+st.write(f"""
+    Siz **{clf_name}** klassifikatoridan foydalyapsiz
+""")
+
+if clf_name == 'SVM':
+    clf = SVC(C=C)
+elif clf_name == 'KNN';
+    clf =KNeighborsClassifier(n_neighbors=K)
+else:
+    clf = RandomForestClassifier(n_estimators=max_depth,
+        max_depth=n_estimators, random_state=42)
